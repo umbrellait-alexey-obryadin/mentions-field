f4f35fc9ac9c99cda70c7441469f98df506f7616
Alexey Obryadin <alexey.obryadin@umbrellait.com>
2025-06-09 20:48:38 +0300

    refactor: dev jsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,16 +13,20 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 if (process.env.NODE_ENV === 'development') {
   root.render(
     <React.StrictMode>
-      <MentionsField
-        onChange={console.log}
-        options={[
-          { label: 'Option 1', value: 'option-1' },
-          { label: 'Option 2', value: 'option-2' },
-          { label: 'Option 3', value: 'option-3' },
-          { label: 'Option 4', value: 'option-4' },
-          { label: 'Option 5', value: 'option-5' }
-        ]}
-      />
+      <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
+        <h1>MentionsField</h1>
+        <p>Для ввода тега по умолчанию используется @</p>
+        <MentionsField
+          onChange={console.log}
+          options={[
+            { label: 'Option 1', value: 'option-1' },
+            { label: 'Option 2', value: 'option-2' },
+            { label: 'Option 3', value: 'option-3' },
+            { label: 'Option 4', value: 'option-4' },
+            { label: 'Option 5', value: 'option-5' }
+          ]}
+        />
+      </div>
     </React.StrictMode>
   )
 }
